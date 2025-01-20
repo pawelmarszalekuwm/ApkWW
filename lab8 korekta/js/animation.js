f07624bbe1244d94e2d8c6159faea899f3da816a
pawelmarszalekuwm <115549330+pawelmarszalekuwm@users.noreply.github.com>
2025-01-20 22:45:44 +0100

    Delete lab8 korekta directory

--- lab8 korekta/js/animation.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    const oscarImage = $('#oscar');
-
-    oscarImage.on('click', function () {
-        $(this).toggleClass('powiekszony'); // Przełączenie klasy powiększenia
-    });
-});
-
